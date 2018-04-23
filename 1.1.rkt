4f9ff91f70082f9a0fa9aca76d86884548d9e95e
jlollis <julielollis@gmail.com>
2018-04-23 17:35:42 -0400

    update

--- 1.1.rkt
+++ 1.1.rkt
@@ -2,35 +2,48 @@
 
 ;;; Exercise 1.1
 
-10 ;10
+10
+;10
 
-(+ 5 3 4) ;12
+(+ 5 3 4)
+;12
 
-(- 9 1) ;8
+(- 9 1)
+;8
 
-(/ 6 2) ;3
+(/ 6 2)
+;3
 
-(+ (* 2 4) (- 4 6)) ;6
+(+ (* 2 4) (- 4 6))
+;6
 
-(define a 3) ;Value: a
+(define a 3)
+;Value: a
 
-(define b (+ a 1)) ;Value: b
+(define b (+ a 1))
+;Value: b
 
-(+ a b (* a b)) ;19
+(+ a b (* a b))
+;19
 
-(= a b) ;#f
+(= a b)
+;#f
 
-(if (and (> b a) (< b (* a b))) ;4
+(if (and (> b a) (< b (* a b)))
  b
  a)
+;4
 
-(cond ((= a 4) 6) ;16
+(cond ((= a 4) 6)
  ((= b 4) (+ 6 7 a))
  (else 25))
+;16
 
-(+ 2 (if (> b a) b a)) ;6 
+(+ 2 (if (> b a) b a))
+;6 
 
-(* (cond ((> a b) a) ;16
+(* (cond ((> a b) a)
  ((< a b) b)
  (else -1))
  (+ a 1))
+;16
