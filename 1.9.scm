3d683ffd8ffd0bcbf83f095791d1599b2747225e
jlollis <julielollis@gmail.com>
2018-04-25 13:07:13 -0400

    change file extensions and add 1.9.txt solution

--- 1.9.scm
+++ 1.9.scm
@@ -1,10 +1,21 @@
 #lang sicp
 
+; Exercise 1.9: See 1.9.text for answer. (Its long and I don't feel like commenting out every line...)
+
+; Case 1:
 (define (+ a b)
   (if (= a 0)
       b
       (+ (dec a) (inc b))))
 
-; (enter (+ <value> <value>) to test
+; Case 2:
+; (define (+ a b)
+;   (if (= a 0)
+;       b
+;       (+ (dec a) (inc b))))
+
+
+
+; Enter (+ <value> <value>) to test
 
 
