1b0d21d8b3a20acec8f1aac204e953af10ca8646
jlollis <julielollis@gmail.com>
2018-05-06 14:56:03 -0400

    changed 1.12

--- 1.12.scm
+++ 1.12.scm
@@ -1,20 +1,20 @@
-#lang sicp
+#lang scheme
 
-; Exercise 1.12. The following pattern of numbers is called Pascal’s triangle.
+#| Exercise 1.12. The following pattern of numbers is called Pascal’s triangle.
 
-;          1
-;         1 1
-;        1 2 1
-;       1 3 3 1
-;      1 4 6 4 1
+          1
+         1 1
+        1 2 1
+       1 3 3 1
+      1 4 6 4 1
 
-; The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum of the
-; two numbers above it. Write a procedure that computes elements of Pascal’s triangle by means of a
-; recursive process.
+ The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum of the
+ two numbers above it. Write a procedure that computes elements of Pascal’s triangle by means of a
+ recursive process.|#
 
-; Answer:
+;;; Answer:
 
-; Enter (pascals-triangle <value>) to test
+;;; Enter (pascals-triangle <value>) to test
 
 (define (pascal x y)
   (define current null)
@@ -33,3 +33,5 @@
     (for ([y (in-range 0 (add1 x))])
       (printf "~a " (pascal x y)))
     (newline)))
+
+(pascals-triangle 5)
