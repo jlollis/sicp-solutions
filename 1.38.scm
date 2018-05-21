b0696a9c2a532556c99fd9501cf24e21cfa7380f
jlollis <julielollis@gmail.com>
2018-05-21 07:09:54 -0400

    add 1.38

--- 1.38.scm
+++ 1.38.scm
@@ -7,70 +7,35 @@ the natural logarithms. In this fraction, the N_i are all 1, and the D_i are suc
 
 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, ....
 
-Write a program that uses your cont-frac procedure from exercise 1.37 to approximate e, based on Euler’s expansion. 
+Write a program that uses your cont-frac procedure from exercise 1.37 to approximate e, based on Euler’s expansion.
+
+Euler's expansion: https://en.wikipedia.org/wiki/E_(mathematical_constant)
 |#
 
-;;;SECTION 1.3.3
+; from 1.37
+(define (cont-frac n d k)
+  (define (k-summ i)
+    (if (< i k)
+        (/ (n i) (+ (d i) (k-summ (+ i 1))))
+        (/ (n i) (d i))))
+  (k-summ 1))
 
-;; Half-interval method
+;(cont-frac (lambda (i) 1.0)
+;           (lambda (i) 1.0)
+;           10)
 
-(define (search f neg-point pos-point)
-  (let ((midpoint (average neg-point pos-point)))
-    (if (close-enough? neg-point pos-point)
-        midpoint
-        (let ((test-value (f midpoint)))
-          (cond ((positive? test-value)
-                 (search f neg-point midpoint))
-                ((negative? test-value)
-                 (search f midpoint pos-point))
-                (else midpoint))))))
+; approximate e based on Euler's expansion
 
-(define (close-enough? x y)
-  (< (abs (- x y)) 0.001))
+(define (n i)
+  1)
+(define (divides? a b)
+  (= 0 (remainder b a)))
+(define (d i)
+  (if (divides? 3 (+ 1 i))
+      (* 2 (/ 3 (+ 1 i)))
+      1))
 
-(define (half-interval-method f a b)
-  (let ((a-value (f a))
-        (b-value (f b)))
-    (cond ((and (negative? a-value) (positive? b-value))
-           (search f a b))
-          ((and (negative? b-value) (positive? a-value))
-           (search f b a))
-          (else
-           (error "Values are not of opposite sign" a b)))))
+(define e
+  (+ 2.0 (cont-frac n d 10))) ; appx to 10 terms in expansion
 
-
-;: (half-interval-method sin 2.0 4.0)
-
-;: (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
-;:                       1.0
-;:                       2.0)
-
-
-;; Fixed points
-
-(define tolerance 0.00001)
-
-(define (fixed-point f first-guess)
-  (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-          next
-          (try next))))
-  (try first-guess))
-
-
-;: (fixed-point cos 1.0)
-
-;: (fixed-point (lambda (y) (+ (sin y) (cos y)))
-;:              1.0)
-
-
-(define (sqrt x)
-  (fixed-point (lambda (y) (/ x y))
-               1.0))
-
-(define (sqrt x)
-  (fixed-point (lambda (y) (average y (/ x y)))
-               1.0))
+; enter e in REPL to test
