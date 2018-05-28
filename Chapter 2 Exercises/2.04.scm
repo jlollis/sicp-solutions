748fce69fc2c57d3157157788ca4cae2b2e18a62
jlollis <julielollis@gmail.com>
2018-05-28 18:39:40 -0400

    update 2.03, add solution 2.04

---
+++ Chapter 2 Exercises/2.04.scm
@@ -0,0 +1,39 @@
+#lang sicp
+
+#|
+Here is an alternative procedural representation of pairs. For this representation, verify
+that (car (cons x y)) yields x for any objects x and y.
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+(define (car z)
+  (z (lambda (p q) p)))
+
+What is the corresponding definition of cdr? (Hint: To verify that this works, make use of the
+substitution model of section 1.1.5.) 
+|#
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+; verify (car (cons x y) yields x for any objects x or y
+(car (cons 3 9))
+(car (cons "42" "lol"))
+(car (cons "six" 6))
+
+; and so on...
+
+; definition of cdr, using substitution model
+
+(define (cdr z)
+  (z (lambda (p q) p)))
+
+(car (cons 3 9))
+(car (cons "42" "lol"))
+(car (cons "six" 6))
+
+; checks out...
+; that's how Scheme was written to work, no surprises here...
