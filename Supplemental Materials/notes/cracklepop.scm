1573994a2a6e4d0f9d6a978bedc2c77dc4043a2c
jlollis <julielollis@gmail.com>
2018-05-28 22:07:22 -0400

    add cracklepop

--- Supplemental Materials/notes/cracklepop.scm
+++ Supplemental Materials/notes/cracklepop.scm
@@ -4,7 +4,6 @@
 Write a program that prints out the numbers 1 to 100 (inclusive). If the number is divisible by 3, print Crackle instead of the number. If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. You can use any language.
 |#
 
-
 (define n 0)
 (do ()
     ((= n 101))
