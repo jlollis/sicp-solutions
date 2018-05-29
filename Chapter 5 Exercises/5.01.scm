b8b789e8e3a1a5211fffe82607a4c1a3c25e080d
jlollis <julielollis@gmail.com>
2018-05-28 20:03:33 -0400

    add folders for chapters 3, 4 and 5

---
+++ Chapter 5 Exercises/5.01.scm
@@ -0,0 +1,6 @@
+#lang sicp
+
+#|
+Exercise 5.1. Design a register machine to compute factorials using the iterative algorithm specified
+by the following procedure. Draw data-path and controller diagrams for this machine. [See book]
+|#
